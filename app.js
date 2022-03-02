9d0eb9b036e51c05c2c9a57dc30e0e84a1318593
naka848 <n.moeko1966@gmail.com>
2022-03-02 16:16:29 +0900

    セクション2

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("ここにコードを追加します。");
+var userInput;
+var userName;
+if (typeof userInput === "string") {
+    userName = userInput;
+}
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
+}
+generateError('エラーが発生しました', 500);
--- app.ts
+++ app.ts
@@ -1,1 +1,12 @@
-console.log("ここにコードを追加します。");
+let userInput: unknown;
+let userName: string;
+
+if (typeof userInput === "string") {
+  userName = userInput;
+}
+
+function generateError(message: string,code: number){
+    throw{ message:message, errorCode:code} ;
+}
+
+generateError('エラーが発生しました',500);
