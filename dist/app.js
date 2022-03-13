33ac1b824f170d551356ad8fe4e63e361abdadb9
nigaya <n.moeko1966@gmail.com>
2022-03-13 15:46:50 +0900

    セクション７

--- dist/app.js
+++ dist/app.js
@@ -2,13 +2,32 @@
 function merge(objA, objB) {
     return Object.assign(objA, objB);
 }
-const mergedObj = merge({ name: "Max", hobbies: ["Sports"] }, 30);
+const mergedObj = merge({ name: "Max", hobbies: ["Sports"] }, { age: 30 });
 console.log(mergedObj);
 function countAndDescribe(element) {
-    let descriptionText = '値がありません。';
+    let descriptionText = "値がありません。";
     if (element.length > 0) {
-        descriptionText = '値は' + element.length + '個です。';
+        descriptionText = "値は" + element.length + "個です。";
     }
     return [element, descriptionText];
 }
 console.log(countAndDescribe([]));
+function extractAndConvert(obj, key) {
+    return "Value: " + obj[key];
+}
+const result = extractAndConvert({ name: 'Max' }, "name");
+console.log(result);
+class DataStorage {
+    constructor() {
+        this.data = [];
+    }
+    addItem(item) {
+        this.data.push(item);
+    }
+    removeItem(item) {
+        this.data.splice(this.data.indexOf(item), 1);
+    }
+    getItems() {
+        return [...this.data];
+    }
+}
--- src/app.ts
+++ src/app.ts
@@ -20,7 +20,7 @@ function merge<T extends object, U extends object>(objA: T, objB: U) {
 }
 
 // エラー:型 'number' の引数を型 'object' のパラメーターに割り当てることはできません
-const mergedObj = merge({ name: "Max", hobbies: ["Sports"] }, 30);
+const mergedObj = merge({ name: "Max", hobbies: ["Sports"] }, { age: 30 });
 
 // 結果：{name: 'Max', hobbies: Array(1)}
 console.log(mergedObj);
@@ -29,14 +29,46 @@ interface Lengthy {
   length: number;
 }
 
-
-function countAndDescribe<T extends Lengthy>(element: T):[T,string]{
-  let descriptionText = '値がありません。'
-  if(element.length > 0){
-    descriptionText = '値は' + element.length + '個です。';
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = "値がありません。";
+  if (element.length > 0) {
+    descriptionText = "値は" + element.length + "個です。";
   }
-  return [element,descriptionText]
+  return [element, descriptionText];
 }
 
 // console.log(countAndDescribe({length:3}))
-console.log(countAndDescribe([]))
+console.log(countAndDescribe([]));
+
+// obj[key]
+// オブジェクトのプロパティに[]でアクセス
+// JavaScriptの通常の構文
+
+function extractAndConvert<T extends object, U extends keyof T>(
+  obj: T,
+  key: U
+) {
+  return "Value: " + obj[key];
+}
+
+// 2つ目の引数「name」は、1つ目の引数のオブジェクトにプロパティとして存在しないのでエラー
+const result = extractAndConvert({name:'Max'}, "name");
+console.log(result);
+
+class DataStorage{
+  private data = [];
+
+  addItem(item){
+    this.data.push(item);
+  }
+
+  removeItem(item){
+    // splice(配列を変更する先頭の位置, 取り除く古い要素の個数)
+    this.data.splice(this.data.indexOf(item),1)
+  }
+
+  getItems(){
+    // 他のやり方では呼び出せない？もしくは適切でない？
+    return [...this.data];
+  }
+}
