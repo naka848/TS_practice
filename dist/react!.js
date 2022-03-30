83d48c96839236094f1d1f6b43f00dac0d73b690
nigaya <n.moeko1966@gmail.com>
2022-03-30 14:26:26 +0900

    セクション８

--- dist/react!.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-{
-    const add = function (n, m) {
-        return n + m;
-    };
-    const subtract = (n, m) => n - m;
-    console.log(add(1, 2));
-    console.log(subtract(7, 2));
-}
