167a01d0f23075b02ac72a58baf0fa2692307316
nigaya <n.moeko1966@gmail.com>
2022-04-03 13:20:53 +0900

    セクション８

--- dist/app.js
+++ dist/app.js
@@ -5,18 +5,51 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-function Decochan() {
+function Logger(logString) {
     return function (constructor) {
-        console.log("mama");
-        console.log(constructor);
+    };
+}
+function WithTemplate(template, hookId) {
+    return function (constructor) {
+        const hookEl = document.getElementById(hookId);
+        const p = new constructor();
+        if (hookEl) {
+            hookEl.innerHTML = template;
+            hookEl.querySelector("h1").innerHTML = p.name;
+        }
     };
 }
 let Deco = class Deco {
     constructor() {
         this.name = "Max";
-        console.log("mimi");
     }
 };
 Deco = __decorate([
-    Decochan()
+    Logger("ログ出力中…"),
+    WithTemplate("<h1>Personオブジェクト</h1>", "app")
 ], Deco);
+function Log(target, propertyName) {
+    console.log("Property デコレータ");
+    console.log(target);
+    console.log(propertyName);
+}
+class Product {
+    constructor(t, p) {
+        this.title = t;
+        this._price = p;
+    }
+    set price(val) {
+        if (val > 0) {
+            this._price = val;
+        }
+        else {
+            throw new Error('不正な価格です。０以下は設定できません。');
+        }
+    }
+    getPriceWithTax(tax) {
+        return this._price * (1 + tax);
+    }
+}
+__decorate([
+    Log
+], Product.prototype, "title", void 0);
--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,67 @@
-function Decochan() {
-  return function(constructor: Function){
-    console.log("mama");
-    console.log(constructor);
-  }
-
+function Logger(logString: string) {
+  // console.log("Logger ファクトリ");
+  return function (constructor: Function) {
+    // console.log(logString);
+    // console.log(constructor);
+  };
 }
 
-@Decochan()
+function WithTemplate(template: string, hookId: string) {
+  // console.log("WithTemplate ファクトリ");
+  return function (constructor: any) {
+    // console.log("テンプレートを出力中…");
+    const hookEl = document.getElementById(hookId);
+    const p = new constructor();
+    // console.log(p);
+    if (hookEl) {
+      // hookEl.textContent = template;
+      // console.log(hookEl)
+      // console.log(hookEl.textContent)
+      // console.log(hookEl.innerHTML)
+      hookEl.innerHTML = template;
+      // hookEl.querySelector('h1')!.textContent = p.name
+      hookEl.querySelector("h1")!.innerHTML = p.name;
+    }
+  };
+}
+
+@Logger("ログ出力中…")
+@WithTemplate("<h1>Personオブジェクト</h1>", "app")
 class Deco {
   name = "Max";
   constructor() {
-    console.log("mimi");
+    // console.log("mimi");
+  }
+}
+
+// ----------------------------------------
+
+function Log(target:any,propertyName:string|symbol){
+  console.log("Property デコレータ")
+  console.log(target)
+  console.log(propertyName)
+}
+
+
+class Product {
+  @Log
+  title: string;
+  private _price: number;
+
+  set price(val: number) {
+    if (val > 0) {
+      this._price = val;
+    }else{
+      throw new Error('不正な価格です。０以下は設定できません。')
+    }
+  }
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+
+  getPriceWithTax(tax: number) {
+    return this._price * (1 + tax);
   }
 }
