678acd782434db671b7376b62af6f761e6b762a8
nigaya <n.moeko1966@gmail.com>
2022-04-04 23:04:52 +0900

    セクション９

--- dist/app.js
+++ dist/app.js
@@ -5,6 +5,16 @@ class ProjectInput {
         console.log(this.templateElement);
         this.hostElement = document.getElementById("app");
         console.log(this.hostElement);
+        const importedNode = document.importNode(this.templateElement.content, true);
+        console.log("importedNode");
+        console.log(importedNode);
+        this.element = importedNode.firstElementChild;
+        console.log("this.element");
+        console.log(this.element);
+        this.attach();
+    }
+    attach() {
+        this.hostElement.insertAdjacentElement("afterbegin", this.element);
     }
 }
 const p = new ProjectInput();
